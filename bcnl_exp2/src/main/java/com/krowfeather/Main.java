4eef26bd0797b2c440d436fc201ed22a80ab8f52
KrowFeather <2625056626@qq.com>
2025-12-20 02:46:00 +0800

    ok

--- bcnl_exp2/src/main/java/com/krowfeather/Main.java
+++ bcnl_exp2/src/main/java/com/krowfeather/Main.java
@@ -2,7 +2,7 @@ package com.krowfeather;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("first commit");
-        System.out.println("second commit");
+        System.out.println("first commit!");
+        System.out.println("second commit!");
     }
 }
